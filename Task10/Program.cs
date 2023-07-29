3384d2f48e1e8de162fd4945c4eaffc49212d15d
Fedyunin <ivan-fed_87@mail.ru>
2023-07-29 18:58:06 +0300

    done 10 Task

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int n1 = number % 100;
+int n2 = n1 / 10;
+Console.Write(n2);
+
+
