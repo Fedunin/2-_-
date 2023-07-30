efd3159a7c7536d62ebe6e8e17cfb3114dd75fc1
Fedyunin <ivan-fed_87@mail.ru>
2023-07-30 09:33:06 +0300

    done 13 Task

---
+++ Task13/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int m = (n % 10);
+// int e = (m % 1000);
+// if (n % 100 == n)
+//     Console.WriteLine("третьей цифры нет");
+// else if (n < 999)
+//     Console.WriteLine(m);
+// else
+//    Console.WriteLine(e); 
+
+Console.Clear();
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result =0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+
+        result = (a / c) % 10;
+    }
+return result;
+}
